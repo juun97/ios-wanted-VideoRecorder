608933a896ca2277bf0ccc1bc9b72dfe5d8eae7c
Jun <59365211+juun97@users.noreply.github.com>
2023-06-05 22:10:59 +0900

    feat : Storyboard 삭제

---
+++ VideoRecorder/VideoRecorder/Application/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  VideoRecorder
+//
+//  Created by 김성준 on 2023/06/05.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let rootViewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ VideoRecorder/VideoRecorder/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoRecorder
+//
+//  Created by 김성준 on 2023/06/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
